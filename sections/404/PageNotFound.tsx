ad9b3c827804c0f25812c3ff70957a66d1f155b1
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-03-12 18:16:12 -0300

    refactor(404): run lint

--- sections/404/PageNotFound.tsx
+++ sections/404/PageNotFound.tsx
@@ -1,28 +1,39 @@
 import { asset } from "$fresh/runtime.ts";
 
 export interface Props {
-    /**
-     * @format html
-    */
-    title: string;
-    /**
-     * @format html
-    */
-    subTitle: string;
-    link?: string;
+  /**
+   * @format html
+   */
+  title: string;
+  /**
+   * @format html
+   */
+  subTitle: string;
+  link?: string;
 }
 
 export default function PageNotFound({ title, subTitle, link }: Props) {
-    return (
-        <div class="mainContainer flex flex-col-reverse py-20">
-            <div>
-                <img class="mx-auto" src={asset("/image/404.png")} width={400} height={400} />
-            </div>
+  return (
+    <div class="mainContainer flex flex-col-reverse py-20">
+      <div>
+        <img
+          class="mx-auto"
+          src={asset("/image/404.png")}
+          width={400}
+          height={400}
+        />
+      </div>
 
-            <div>
-                <span class="text-5xl leading-[52px] md:text-6xl" dangerouslySetInnerHTML={{__html: title}} />
-                <span class="text-base block pt-3 py-6 font-light md:text-xl" dangerouslySetInnerHTML={{__html: subTitle}} />
-            </div>
-        </div>
-    )
+      <div>
+        <span
+          class="text-5xl leading-[52px] md:text-6xl"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+        <span
+          class="text-base block pt-3 py-6 font-light md:text-xl"
+          dangerouslySetInnerHTML={{ __html: subTitle }}
+        />
+      </div>
+    </div>
+  );
 }
